d8b9bac56123f2810163af706dc9d8c904a1da98
Mustafa Kamaal <mkamaalg4@gmail.com>
2019-11-23 14:00:48 +0530

    Lab04 assignment added

---
+++ Mustafa_Kamaal_34_Lab4_Q1.py
@@ -0,0 +1,9 @@
+#Name- MOHD MUSTAFA KAMAAL
+#Roll No:- 34
+#En No:- A180571
+list= [5, 10, 7, 4, 15, 3]
+print("The given list is-:",list)
+def convert(list):
+ return tuple(list)
+print("After converting List to Tuple\nThe Output is-:")
+print(convert(list))
